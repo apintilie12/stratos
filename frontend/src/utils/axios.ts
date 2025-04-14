1cc6f3770e5b0fd7a56d0fbb8a8a423be30adf1b
apintilie12 <apintilie12@gmail.com>
2025-04-14 10:03:16 +0300

    Added AuthFilter

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,8 +1,22 @@
-// src/lib/axios.ts
 import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL,
 });
+
+// Add a request interceptor to the default Axios instance
+api.interceptors.request.use(
+    (config) => {
+        const token = sessionStorage.getItem('token');
+        if (token) {
+            config.headers = config.headers || {};
+            config.headers.set('Authorization', `Bearer ${token}`);
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
